52577d9d36122e308573d6044b6217a37d53e42b
Joshua Shadik <shadikjo@arctic.cse.msu.edu>
2014-04-02 02:17:22 -0400

    hw10

--- imageapp/image.py
+++ imageapp/image.py
@@ -2,6 +2,8 @@
 
 images = {}
 
+temp_images = {}
+
 
 def add_image(data, name, description):
     if images:
@@ -12,9 +14,16 @@ def add_image(data, name, description):
     images[image_num] = [data, name, description]
     return image_num
 
+def add_temp(data):
+	temp_images[0] = data
+	return
+
+def get_temp():
+	return temp_images[0]
+
 def get_image(num):
     return images[num][0]
 
 def get_latest_image():
     image_num = max(images.keys())
-    return images[image_num][0]
+    return images[image_num]
